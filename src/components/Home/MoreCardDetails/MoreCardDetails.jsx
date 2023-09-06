79f85ed2a44bf6dc7ee5f1361dfe78f17053ce44
sohag838 <95273824+sohag838@users.noreply.github.com>
2023-09-06 11:20:34 +0600

    more details page update

--- src/components/Home/MoreCardDetails/MoreCardDetails.jsx
+++ src/components/Home/MoreCardDetails/MoreCardDetails.jsx
@@ -3,12 +3,12 @@ import { useLoaderData } from "react-router-dom";
 const MoreCardDetails = () => {
   const service = useLoaderData();
 
-  const { name, location } = service.data[0];
+  const { name, location, desc } = service.data[0];
   return (
     <div>
       <h2>{name}</h2>
       <p>{location}</p>
-      <p>hi</p>
+      <p>{desc}</p>
     </div>
   );
 };
